8457b99ae36d533926ed75955b1283ff8bd31f16
mihaGP800 <mihagoroh@gmail.com>
2022-08-19 15:21:59 +0300

    js: deep equal

---
+++ 12_deepEqual.js
@@ -0,0 +1,31 @@
+function deepEqual(a, b) {
+
+    if (Number.isNaN(a) && Number.isNaN(b)) return true
+
+    if (typeof a !== typeof b) return false
+
+    if (typeof a !== 'object' || a === null || b === null) return a === b
+
+    if (Object.keys(a).length !== Object.keys(b).length) return false
+
+
+    for (const key of Object.keys(a)) {
+        if (!deepEqual(a[key], b[key])) return false
+    }
+
+    return true
+
+}
+
+const source = {a: 1, b: {c: 1}}
+const test1 = {a: 1, b: {c: 1}}
+const test2 = {a: 1, b: {c: 2}}
+
+
+console.log(deepEqual(source, test1)) // -> true
+console.log(deepEqual(source, test2)) // -> false
+console.log('NaN, NaN', deepEqual(NaN, NaN)) // -> true
+console.log('function, function', deepEqual(() => {}, () => {})) // -> false
+console.log('string', deepEqual('test', 'test!')) // -> false
+console.log('undefined undefined', deepEqual()) // -> true
+console.log('different types', deepEqual(42, '42')) // -> true
